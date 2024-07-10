dcbb7ac1351cf82f1b1f7f5e7ed095f1be7007ce
Katherine <katabeth.ch@gmail.com>
2024-07-10 15:22:55 +0100

    Created Project

---
+++ AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/AudumblaWorldJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.audumbla.audumblaworldjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudumblaWorldJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudumblaWorldJpaApplication.class, args);
+	}
+
+}
---
+++ AudumblaWorldJPA/src/test/java/com/sparta/audumbla/audumblaworldjpa/AudumblaWorldJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.audumbla.audumblaworldjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudumblaWorldJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
