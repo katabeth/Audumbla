cf079ad0dd8a23f1b415156e262978be4a73a6cf
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-10 16:52:59 +0100

    fix: added extends JpaRepository to our repositories interfaces

--- AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/repositories/CityRepository.java
+++ AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/repositories/CityRepository.java
@@ -1,7 +1,9 @@
 package com.sparta.audumbla.audumblaworldjpa.repositories;
 
+import com.sparta.audumbla.audumblaworldjpa.entities.City;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CityRepository {
+public interface CityRepository extends JpaRepository<City, Integer> {
 }
