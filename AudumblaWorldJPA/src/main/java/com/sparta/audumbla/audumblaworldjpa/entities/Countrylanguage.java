84f0dfc41940a217bea3b94e6114825e41c5ee36
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-10 16:11:16 +0100

    Created repositories, added the service and entities

---
+++ AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/entities/Countrylanguage.java
@@ -0,0 +1,64 @@
+package com.sparta.audumbla.audumblaworldjpa.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "countrylanguage", schema = "world")
+public class Countrylanguage {
+    @EmbeddedId
+    private CountrylanguageId id;
+
+    @MapsId("countryCode")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ColumnDefault("''")
+    @JoinColumn(name = "CountryCode", nullable = false)
+    private Country countryCode;
+
+    @NotNull
+    @ColumnDefault("'F'")
+    @Lob
+    @Column(name = "IsOfficial", nullable = false)
+    private String isOfficial;
+
+    @NotNull
+    @ColumnDefault("0.0")
+    @Column(name = "Percentage", nullable = false, precision = 4, scale = 1)
+    private BigDecimal percentage;
+
+    public CountrylanguageId getId() {
+        return id;
+    }
+
+    public void setId(CountrylanguageId id) {
+        this.id = id;
+    }
+
+    public Country getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(Country countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getIsOfficial() {
+        return isOfficial;
+    }
+
+    public void setIsOfficial(String isOfficial) {
+        this.isOfficial = isOfficial;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+}
