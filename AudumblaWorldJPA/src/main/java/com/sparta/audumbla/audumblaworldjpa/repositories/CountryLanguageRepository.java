e170740b87527ce44247523dc63139d1fcea02da
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-11 10:31:11 +0100

    Fix: issue with type of JpaRepository interface - as our Countrylanguage has a composite key it has to be Countrylanguage, CountrylanguageId

--- AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/repositories/CountryLanguageRepository.java
+++ AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/repositories/CountryLanguageRepository.java
@@ -1,9 +1,10 @@
 package com.sparta.audumbla.audumblaworldjpa.repositories;
 
 import com.sparta.audumbla.audumblaworldjpa.entities.Countrylanguage;
+import com.sparta.audumbla.audumblaworldjpa.entities.CountrylanguageId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CountryLanguageRepository extends JpaRepository<Countrylanguage, Integer> {
+public interface CountryLanguageRepository extends JpaRepository<Countrylanguage, CountrylanguageId> {
 }
