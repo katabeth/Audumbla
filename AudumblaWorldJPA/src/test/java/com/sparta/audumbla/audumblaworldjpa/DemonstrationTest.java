224df4da547d62fd12e2bf4cbc7a64097d21b71d
Gilbert Reid <greid@spartaglobal.com>
2024-07-17 13:47:03 +0100

    Updating to include one to manies

--- AudumblaWorldJPA/src/test/java/com/sparta/audumbla/audumblaworldjpa/DemonstrationTest.java
+++ AudumblaWorldJPA/src/test/java/com/sparta/audumbla/audumblaworldjpa/DemonstrationTest.java
@@ -5,23 +5,21 @@ import com.sparta.audumbla.audumblaworldjpa.entities.City;
 import com.sparta.audumbla.audumblaworldjpa.entities.Country;
 import com.sparta.audumbla.audumblaworldjpa.entities.Countrylanguage;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.Modifying;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DemonstrationTest extends WorldServiceTest {
 
-    @Disabled
     @Test
-    @DisplayName("Getting all Countries from The database")
+    @DisplayName("Z Getting all Countries from The database")
     void getAllCountriesDemonstration() {
         List<Country> countries = worldService.getAllCountries();
         for(Country country : countries){
@@ -30,10 +28,9 @@ public class DemonstrationTest extends WorldServiceTest {
         System.out.println("found " + countries.size() + " Countries");
     }
 
-    @Disabled
     @Transactional
     @Test
-    @DisplayName("Getting all Cities from The database")
+    @DisplayName("Z Getting all Cities from The database")
     void getAllCitiesDemonstration() {
         List<City> cities = worldService.getAllCities();
         for(City city : cities){
@@ -43,9 +40,8 @@ public class DemonstrationTest extends WorldServiceTest {
     }
 
 
-    @Disabled
     @Test
-    @DisplayName("Getting all CountryLanguages from The database")
+    @DisplayName("Z Getting all CountryLanguages from The database")
     void getAllCountriesLanguagesDemonstration() {
         List<Countrylanguage> languages = worldService.getAllCountryLanguages();
         for(Countrylanguage language : languages){
@@ -56,8 +52,9 @@ public class DemonstrationTest extends WorldServiceTest {
 
     @Test
     @Order(1)
-    @DisplayName("Adding NewLand")
+    @DisplayName("A Adding NewLand")
     void addingNewCountryDemonstration(){
+        System.out.println("Adding country newland");
         Country newLand = new Country();
         newLand.setCode("NLE");
         newLand.setName("NewLand");
@@ -79,7 +76,7 @@ public class DemonstrationTest extends WorldServiceTest {
 
     @Test
     @Order(2)
-    @DisplayName("Finding our new country")
+    @DisplayName("B Finding our new country")
     void findingCountryByIdDemo(){
         Country newLand = worldService.getCountryByCountryCode("NLE").orElseThrow();
         System.out.println(newLand);
@@ -87,19 +84,85 @@ public class DemonstrationTest extends WorldServiceTest {
 
     @Test
     @Order(3)
-    @DisplayName("Deleting our New Country")
-    void deletingCountryByIdDemo(){
-        worldService.deleteCountryByCountryCode("NLE");
+    @DisplayName("C Updating our New Country")
+    void updatingCountryDemo(){
+        System.out.println("Updating newLand");
+        Country newLand = worldService.getCountryByCountryCode("NLE").orElseThrow();
+        newLand.setHeadOfState("Kat");
+        worldService.updateCountryTable("NLE",newLand);
+
     }
 
     @Test
     @Order(4)
-    @DisplayName("Checking our new country is deleted")
-    void findingCountryByIdShouldFailDemo(){
+    @DisplayName("D Finding our updated country")
+    void findingCountryByIdUpdated(){
+        Country newLand = worldService.getCountryByCountryCode("NLE").orElseThrow();
+        System.out.println(newLand);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("E Deleting our New Country")
+    void deletingCountryByIdDemo(){
+        System.out.println("Deleting Newland");
+        worldService.deleteCountryByCountryCode("NLE");
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("F Checking our country is deleted")
+    void findingCountryByIdDeleted(){
         try{
             Country newLand = worldService.getCountryByCountryCode("NLE").orElseThrow();
         } catch (NoSuchElementException e){
             System.out.println("This country doesnt exist");
+        }
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("Z Getting Paris By Name")
+    void getAllCountriesDemo(){
+        City paris = worldService.getCitiesByName("Paris").getFirst();
+        System.out.println(paris);
+    }
+
+    @Test
+    @DisplayName("Z Getting the 5 smallest districts")
+    void getSmallestDistricts(){
+        List<String> districts = worldService.getFiveSmallestDistricts();
+        System.out.println(districts);
+    }
+
+    @Test
+    @DisplayName("Z Getting the Main Language Speakers")
+    void getMainLanguageSpeaker(){
+        int speakers = worldService.getCountriesMainLanguageSpeakers("BRA");
+        System.out.println("brazil main language speakers " + speakers);
+    }
+
+    @Test
+    @DisplayName("Z Getting the country with most cities")
+    void getMostCitiesCount(){
+        Map.Entry<Country,Long> output = worldService.findCountryWithMostCities();
+        System.out.println(output.getValue());
+        System.out.println(output.getKey().getName());
+    }
+
+    @Test
+    @DisplayName("Z Getting percentage of population in a countries main city")
+    void getPopulationPercentage(){
+        BigDecimal pop = worldService.calculatePopulationPercentageInLargestCityByCountryName("France");
+        System.out.println("percentage in largest city in france : " + pop);
+    }
+
+    @Test
+    @DisplayName("Z Getting the countries with no head of state")
+    void getNoHeadOfStateCountries(){
+        List<Country> headless = worldService.getCountriesWithoutHeadOfState();
+        for(Country country : headless){
+            System.out.println(country);
         }
     }
 
