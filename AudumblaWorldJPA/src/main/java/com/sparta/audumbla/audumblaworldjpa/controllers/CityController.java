1137bf39545bf9f9675de5ea8ee4087f8c9ffac5
Patrik Turcani <pturcani@spartaglobal.com>
2024-07-18 11:37:25 +0100

    refactoring

--- AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/controllers/CityController.java
+++ AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/controllers/CityController.java
@@ -10,9 +10,11 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -54,18 +56,65 @@ public class CityController {
     public EntityModel<Optional<City>> getCityById(@PathVariable Integer id) {
         Optional<City> city = worldService.getCitiesByID(id);
         return EntityModel.of(city,
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CityController.class).getCityById(id)).withSelfRel(),
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CityController.class).getAllCities()).withRel("cities"),
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CountryController.class).getCountriesByCountryCode(city.orElseThrow().getCountryCode().getCode())).withRel("country"));
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CityController.class)
+                        .getCityById(id))
+                        .withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CityController.class)
+                        .getAllCities())
+                        .withRel("cities"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CountryController.class)
+                        .getCountriesByCountryCode(city.orElseThrow().getCountryCode().getCode()))
+                        .withRel("country"));
     }
 
     @PostMapping
-    public ResponseEntity<City> addCities(@RequestBody City city, HttpServletRequest request) {
+    public EntityModel<City> addCities(@RequestBody City city) {
         if(city.getCountryCode().toString().isEmpty()) {
             worldService.createCountry(city.getCountryCode());
         }
         worldService.createCity(city);
-        URI location = URI.create(request.getRequestURL().toString() + "/" + city.getName());
-        return ResponseEntity.created(location).body(city);
+        return EntityModel.of(city,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CityController.class)
+                        .getCityById(city.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CityController.class)
+                        .getAllCities()).withRel("cities"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CountryController.class)
+                        .getCountriesByCountryCode(city.getCountryCode().getCode()))
+                        .withRel("country"));
+    }
+
+    @PutMapping("/{id}")
+    public EntityModel<City> putCity(@PathVariable Integer id, @RequestBody City city) {
+
+        if (worldService.getCitiesByID(id).isEmpty()){
+            return addCities(city);
+        }
+        worldService.updateCityTable(city.getId(), city);
+        return EntityModel.of(city,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CityController.class)
+                        .getCityById(city.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CityController.class)
+                        .getAllCities()).withRel("cities"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                        .methodOn(CountryController.class)
+                        .getCountriesByCountryCode(city.getCountryCode().getCode()))
+                        .withRel("country"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCity(@PathVariable Integer id) {
+        if (worldService.getCitiesByID(id).isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        worldService.deleteCityById(id);
+        return ResponseEntity.noContent().build();
     }
 }
