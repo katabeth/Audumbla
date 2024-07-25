27d71c74e916dc6a050fc3f7b74fb6e42c382a11
Katherine <katabeth.ch@gmail.com>
2024-07-25 11:15:10 +0100

    added a view all table and trying to add create

--- AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/mvccontrollers/CountryMvcController.java
+++ AudumblaWorldJPA/src/main/java/com/sparta/audumbla/audumblaworldjpa/mvccontrollers/CountryMvcController.java
@@ -1,4 +1,49 @@
 package com.sparta.audumbla.audumblaworldjpa.mvccontrollers;
 
+import com.sparta.audumbla.audumblaworldjpa.entities.Country;
+import com.sparta.audumbla.audumblaworldjpa.service.WorldService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/countries")
 public class CountryMvcController {
+    private final WorldService worldService;
+    @Autowired
+    public CountryMvcController(WorldService worldService) {
+        this.worldService = worldService;
+
+    }
+
+    @GetMapping
+    public String getCountries(@RequestParam(required = false, defaultValue = "") String search, Model model) {
+        if (!search.isEmpty()) {
+            model.addAttribute("countries", worldService.getCountryByCountryCode(search));
+            model.addAttribute("message", "Showing results for: " + search);
+        } else {
+            model.addAttribute("countries", worldService.getAllCountries());
+            model.addAttribute("message", "Showing All Countries");
+        }
+        return "countries/list";
+    }
+
+    @GetMapping("/create")
+    public String createCountry(Model model) {
+        model.addAttribute("country", new Country());
+        return "countries/create";
+    }
+    @PostMapping("/create")
+    public String createCountry(@Valid @ModelAttribute("country") Country country, Errors errors) {
+        if (errors.hasErrors()) {
+            return "countries/create";
+        } else {
+            worldService.createCountry(country);
+            return "redirect:/countries?search=" + country.getCode();
+        }
+    }
+
 }
